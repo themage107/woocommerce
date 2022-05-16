a1dc30ecd91caf714fd436c560e8abb7eeb0ebd8
Adrian Fox <themage107@gmail.com>
2022-05-16 12:38:09 -0500

    order complete

---
+++ auto_complete_woocommerce_orders.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action( 'woocommerce_thankyou', 'custom_woocommerce_auto_complete_order' );
+function custom_woocommerce_auto_complete_order( $order_id ) { 
+    if ( ! $order_id ) {
+        return;
+    }
+
+    $order = wc_get_order( $order_id );
+    $order->update_status( 'completed' );
+
+}
+?>
